a2ab7201b742ee304f9f2a015671a5adc19feeaa
Manjulatha10 <amanjulatha.levontechno@gmail.com>
2024-11-25 18:11:27 +0530

    Marketing

---
+++ src/Components/Marketing/CampaignPerformance/CampaignPerformance.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { GoPlus } from "react-icons/go";
+import { HiOutlineLightBulb } from "react-icons/hi";
+import { AiOutlinePieChart } from "react-icons/ai";
+import { PiHandTap } from "react-icons/pi";
+import { AiOutlineSound } from "react-icons/ai";
+
+const CampaignPerformance = () => {
+  const cardsData = [
+    {
+      title: "Total Campaign",
+      value: 20,
+      percentage: "2%",
+      description: "Since last week",
+      icon: <AiOutlineSound />,
+      borderColor: "var(--primary-green)",
+      iconColor: "var(--primary-green)",
+      iconBackground: "#dffaf0",
+    },
+    {
+      title: "Total Reach",
+      value: "22,500",
+      percentage: "340",
+      description: "Users",
+      icon: <HiOutlineLightBulb />,
+      borderColor: "var(--sky-blue-color)",
+      iconColor: "var(--sky-blue-color)",
+      iconBackground: "#e8f6ff",
+    },
+    {
+      title: "Engagement Rate",
+      value: "15%",
+      percentage: "+165",
+      description: "New",
+      icon: <PiHandTap />,
+      borderColor: "var(--light-orange-color)",
+      iconColor: "#FFD66B",
+      iconBackground: "#FFF4DB",
+    },
+    {
+      title: "Conversion Rate",
+      value: "5%",
+      percentage: "1%",
+      description: "Since last week",
+      icon: <AiOutlinePieChart />,
+      borderColor: "var(--sky-blue-color)",
+      iconColor: "var(--sky-blue-color)",
+      iconBackground: "#e8f6ff",
+    },
+  ];
+
+  return (
+    <div className="container">
+      <div className="d-flex mt-4 justify-content-between campaign-performance-head">
+        <div>
+          <h3>Marketing Overview</h3>
+          <p>There is the latest update for the last 7 days. Check now.</p>
+        </div>
+        <div>
+          <button className="rfh-basic-button">
+            <GoPlus size={20} /> Create Campaign
+          </button>
+        </div>
+      </div>
+
+      <div className="row campaign-performance-container">
+        {cardsData.map((card, index) => (
+          <div className="col-lg-3 col-md-6 col-sm-12 mt-2" key={index}>
+            <div
+              className="campaign-performance-card"
+              style={{ borderRightColor: card.borderColor }}
+            >
+              <div className="d-flex justify-content-between align-items-center">
+                <p>{card.title}</p>
+                <span
+                  className="campaign-performance-icon"
+                  style={{
+                    color: card.iconColor,
+                    backgroundColor: card.iconBackground,
+                  }}
+                >
+                  {card.icon}
+                </span>
+              </div>
+              <h3>{card.value}</h3>
+              <div className="campaign-performance-stats">
+                <p>
+                  <span className="campaign-performance-percentage">
+                    {card.percentage}
+                  </span>{" "}
+                  {card.description}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CampaignPerformance;
--- src/Route.js
+++ src/Route.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react'
-import {BrowserRouter, Route,Routes,useNavigate} from "react-router-dom"
-import UserAquisitionPage from './Pages/UserDashboards/UserAquisitionPage'
+import React, { Component } from "react";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import UserAquisitionPage from "./Pages/UserDashboards/UserAquisitionPage";
+import CampaignPerformancePage from "./Pages/Marketing/CampaignPerformancePage";
 export const AppRouter = () => {
   return (
     <BrowserRouter>
-   <Routes>
-    <Route path="/" element={Component=<UserAquisitionPage/>}/>
-   </Routes>
-   </BrowserRouter>
-  )
-}
+      <Routes>
+        <Route path="/" element={(Component = <UserAquisitionPage />)} />
+        <Route
+          path="/campaign-performance-page"
+          element={(Component = <CampaignPerformancePage />)}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
