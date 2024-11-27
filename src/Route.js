5158d45f7b890b9dc1dbc2b74099b5981901f9f1
Manjulatha10 <amanjulatha.levontechno@gmail.com>
2024-11-27 18:17:33 +0530

    Marketing

--- src/Route.js
+++ src/Route.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import UserAquisitionPage from "./Pages/UserDashboards/UserAquisitionPage";
 import CampaignPerformancePage from "./Pages/Marketing/CampaignPerformancePage";
+import PatientAcquisitionPage from "./Pages/Marketing/PatientAcquisitionPage";
+import PatientDetailPage from "./Pages/Marketing/PatientDetailPage";
 export const AppRouter = () => {
   return (
     <BrowserRouter>
@@ -10,6 +12,16 @@ export const AppRouter = () => {
         <Route
           path="/campaign-performance-page"
           element={(Component = <CampaignPerformancePage />)}
+        />
+
+        <Route
+          path="/patient-acquisition-page"
+          element={(Component = <PatientAcquisitionPage />)}
+        />
+
+      <Route
+          path="/patient-detail-page"
+          element={(Component = <PatientDetailPage />)}
         />
       </Routes>
     </BrowserRouter>
