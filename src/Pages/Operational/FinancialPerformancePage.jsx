2c9267f01ce8a3ce383a2afab4b1ecb21b83cd92
manjuajgowda <152592250+manjuajgowda@users.noreply.github.com>
2024-12-04 15:15:23 +0530

    modified

--- src/Pages/Operational/FinancialPerformancePage.jsx
+++ src/Pages/Operational/FinancialPerformancePage.jsx
@@ -1,29 +1,19 @@
 import React from "react";
-import HeaderAdmin from "../../Layout/Header/Index";
-import SidebarAdmin from "../../Layout/Sidebar/Index";
-import "./AppointmentReports.css"
+import "./AppointmentReports.css";
 import FinancialPerformanceCards from "../../Components/Operational/FinancialPerformance/FinancialPerformanceCards";
-import {FinancialPerformanceGraph} from "../../Components/Operational/FinancialPerformance/FinancialPerformanceGraph";
+import { FinancialPerformanceGraph } from "../../Components/Operational/FinancialPerformance/FinancialPerformanceGraph";
 import FinancialPerformanceTable from "../../Components/Operational/FinancialPerformance/FinancialPerformanceTable";
 
 const FinancialPerformancePage = () => {
   return (
     <>
-      <HeaderAdmin />
-      <SidebarAdmin />
-
-      <div className="main-wrapper">
-            <div className="container">
-               <FinancialPerformanceCards />
-               <FinancialPerformanceGraph />
-               <FinancialPerformanceTable />
-            </div>
+      <div className="container">
+        <FinancialPerformanceCards />
+        <FinancialPerformanceGraph />
+        <FinancialPerformanceTable />
       </div>
     </>
   );
 };
 
 export default FinancialPerformancePage;
-
-
-
--- src/Pages/Teleconsultation/ChatPage.jsx
+++ src/Pages/Teleconsultation/ChatPage.jsx
@@ -1,21 +1,13 @@
-
 import React from "react";
-import HeaderAdmin from "../../Layout/Header/Index";
-import SidebarAdmin from "../../Layout/Sidebar/Index";
 import "./teleconsultation.css";
 import MessageChat from "../../Components/Teleconsultation/TechnicalSupport/Chat";
 
 const ChatPage = () => {
   return (
     <>
-      <HeaderAdmin />
-      <SidebarAdmin />
-      <div className="main-wrapper">
-        <MessageChat />
-      </div>
+      <MessageChat />
     </>
   );
 };
 
 export default ChatPage;
-
