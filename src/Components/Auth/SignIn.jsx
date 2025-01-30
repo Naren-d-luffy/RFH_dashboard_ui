11aa62be866750b7391ced53d51afbafef4e10d9
KonaKeerthi <keerthi.levontechno@gmail.com>
2025-01-30 10:22:06 +0530

    working on rba

--- src/Components/Auth/SignIn.jsx
+++ src/Components/Auth/SignIn.jsx
@@ -12,6 +12,7 @@ import login3 from "../../Assets/Images/login-2.png";
 import login4 from "../../Assets/Images/login-4.png";
 import { showErrorMessage, showSuccessMessage } from "../../globalConstant";
 import { jwtDecode } from "jwt-decode"; // Import jwt-decode
+import { useAuth } from "../../AuthContext";
 
 const CustomDot = ({ active }) => (
   <span className={`dot ${active ? "active" : ""}`}></span>
@@ -24,6 +25,8 @@ const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [rememberMe, setRememberMe] = useState(false);
   const navigate = useNavigate();
+  const authContext = useAuth(); 
+  const { syncAuthState } = authContext || {}; // Get syncAuthState safely
 
   const sliderItems = [
     {
@@ -65,6 +68,7 @@ const SignIn = () => {
   };
 
   const loginUser = async (email, password) => {
+    
     try {
       setLoading(true);
       const response = await Instance.post("/admin/login", {
@@ -97,6 +101,10 @@ const SignIn = () => {
           `Welcome, ${response.data.admin.name}!`,
           "You have successfully logged in."
         );
+       
+        // if (typeof syncAuthState === "function") {
+          syncAuthState(); 
+        // } 
         navigate("/user-dashboards/user-aquisition");
       } else {
         console.log("Login failed with error:", response.data.error);
